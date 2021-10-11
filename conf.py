4e855f70b999bb35349fed4935e827555d58ad4c
Olav Vahtras <vahtras@kth.se>
2021-10-11 18:53:29 +0200

    fixup

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Computational Python'
-copyright = u'2019, Olav Vahtras'
+copyright = u'2021, Olav Vahtras'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
