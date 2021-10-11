9d26d687916be094a099ce97115267a7ea78a638
Olav Vahtras <vahtras@kth.se>
2021-10-11 19:18:01 +0200

    remove explicit unicode

--- conf.py
+++ conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Computational Python'
-copyright = u'2021, Olav Vahtras'
+project = 'Computational Python'
+copyright = '2021, Olav Vahtras'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
